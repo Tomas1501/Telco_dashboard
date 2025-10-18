d1663f57ff686a9a5d43499e9ea98c3c30f8fb91
Tomas <tbartkow@gmail.com>
2025-10-18 21:52:02 +0200

    app hide icon

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import streamlit as st
 
+hide_toolbar = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    </style>
+"""
+st.markdown(hide_toolbar, unsafe_allow_html=True)
 st.set_page_config(
     page_title="Telco dashboard",
     # page_icon="📊",
